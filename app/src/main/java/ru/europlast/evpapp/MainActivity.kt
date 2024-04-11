311b6440c8c3c50b21f22e551f2f75690ad6ee44
yudin <brawlakkum@gmail.com>
2024-04-11 15:56:49 +0300

    added binding

--- app/src/main/java/ru/europlast/evpapp/MainActivity.kt
+++ app/src/main/java/ru/europlast/evpapp/MainActivity.kt
@@ -1,20 +1,37 @@
 package ru.europlast.evpapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import ru.europlast.evpapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding from ActivityMain must not be null")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.btnAuthButton.setOnClickListener{
+            testFunction()
+
+        }
+    }
+
+    private fun testFunction() {
+        // TODO: Implement this function later
     }
 }
